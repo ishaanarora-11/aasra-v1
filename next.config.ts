397ecd45075ccb7d3c7401c3ec7dc5a5a9f2c5c8
ishaanarora-11 <ishaan24266@iiitd.ac.in>
2024-11-16 04:07:00 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images:{
+    domains:['www.google.com','static.vecteezy.com','thumbs.dreamstime.com','media.istockphoto.com','loveincorporated.blob.core.windows.net','hips.hearstapps.com','www.eatingbirdfood.com','img.freepik.com']
+  }
+  
+  /* config options here */
+};
+
+
+export default nextConfig;
